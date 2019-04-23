5754156bc867f488cec0eba660ce3b19611b31b6
Rahul Dasgupta <rahul_dasgupta@outlook.com>
2019-04-23 10:49:45 +0530

    Update result.blade.php

--- Laravel/Views/result.blade.php
+++ Laravel/Views/result.blade.php
@@ -3,6 +3,7 @@
 <head>
 <title>Posts</title>
 <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
 <style>
 div {
   border-radius: 5px;
@@ -12,17 +13,68 @@ div {
   margin-bottom: 17px;
   font-size: 18px;
 }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: 'Merienda', cursive;
+  font-size: 14px;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+.button {
+  background-color: #273746;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.button2 {
+  font-size: 13px;
+  font-family: 'Merienda', cursive;
+}
 </style>
 </head>
 <body>
+  <body>
+    <ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="http://127.0.0.1:8000/articles">Articles</a></li>
+    <li><a class="active" href="http://127.0.0.1:8000/edit-records">Edit Publications</a></li>
+    <li><a href="http://127.0.0.1:8000/insert">Post Articles</a></li>
+  </ul>
 @foreach ($users as $user)
 <div>
 {{$user->title}}<br>
-{{$user->tags}}<br>
-{{$user->postdate}}<br>
-{{$user->post}}<br>
-<a href='edit/{{ $user->title }}'>Edit</a><br>
-<a href='delete/{{ $user->title }}'>Delete</a><br>
+<p style="font-size:15px;">{{$user->tags}}<br>
+{{$user->postdate}}</p><hr>
+{{$user->post}}<br><br>
+<button class="button button2" onclick="window.location.href = 'edit/{{ $user->title }}';">Edit</button>
+<button class="button button2" onclick="window.location.href = 'delete/{{ $user->title }}';">Delete</button>
 </div>
 @endforeach
 
