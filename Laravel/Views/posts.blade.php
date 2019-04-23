c2529858adcb56e4c168d44e21b62d04e7c35965
Rahul Dasgupta <rahul_dasgupta@outlook.com>
2019-04-23 10:48:07 +0530

    Update posts.blade.php

--- Laravel/Views/posts.blade.php
+++ Laravel/Views/posts.blade.php
@@ -55,9 +55,44 @@ textarea {
   background-color: #f8f8f8;
   resize: none;
 }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: 'Merienda', cursive;
+  font-size: 14px;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
 </style>
 </head>
 <body>
+  <ul>
+  <li><a href="#home">Home</a></li>
+  <li><a  href="http://127.0.0.1:8000/articles">Articles</a></li>
+  <li><a href="http://127.0.0.1:8000/edit-records">Edit Publications</a></li>
+  <li><a class="active" href="http://127.0.0.1:8000/insert">Post Articles</a></li>
+</ul>
 <div>
   <form action = "/create" method = "post">
     <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
